6f9004f99a0ef744a9a06946f28f97855bcee13f
Preeti <preeti@IL323.local>
2017-11-14 14:08:08 +0530

    initial commit

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './dashboard.css';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div> 
+        hellow world
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
